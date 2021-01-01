460dbfe7b2584916ab3f9bb9160cafbab5037764
Rui Azevedo <ruiazevedo@MBP-de-Rui.lan>
2021-01-01 18:58:24 +0000

    checkpoints & archiving scripts

--- scripts/autotest_archiving.sh
+++ scripts/autotest_archiving.sh
@@ -1,76 +1,33 @@
 #!/bin/bash
 
 ######################################################
-#                         FSYNC                      #
+  #                   ARCHIVE MODE                   #
 ######################################################
 
-~/scripts/autorun_optimizations.sh "#fsync =" "fsync = off" ~/results/settings/fsync fsync.dat
+~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_mode =" "archive_mode = off" ~/results/settings/archive_mode archive_mode_off
+
+~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_mode =" "archive_mode = on" ~/results/settings/archive_mode archive_mode_on.dat
+
+~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_mode =" "archive_mode = always" ~/results/settings/archive_mode archive_mode_always
 
 ######################################################
-#                  SYNCHRONOUS COMMIT                #
+#                    ARCHIVE COMMAND                 #
 ######################################################
 
-~/scripts/autorun_optimizations.sh "#synchronous_commit =" "synchronous_commit = off" ~/results/settings/synchronous_commit synchronous_commit_off
+~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_command =" "archive_command = '%p'" ~/results/settings/archive_command archive_command_p
 
-~/scripts/autorun_optimizations.sh "#synchronous_commit =" "synchronous_commit = remote_write" ~/results/settings/synchronous_commit synchronous_commit_remote_write
+~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_command =" "archive_command = '%f'" ~/results/settings/archive_command archive_command_f
 
-~/scripts/autorun_optimizations.sh "#synchronous_commit =" "synchronous_commit = local" ~/results/settings/synchronous_commit synchronous_commit_local
-
-~/scripts/autorun_optimizations.sh "#synchronous_commit =" "synchronous_commit = remote_apply" ~/results/settings/synchronous_commit synchronous_commit_remote_apply
 
 ######################################################
-#                     WAL SYNC METHOD                #
+#                     ARCHIVE TIMEOUT                #
 ######################################################
 
-~/scripts/autorun_optimizations.sh "#wal_sync_method =" "wal_sync_method = fsync" ~/results/settings/wal_sync_method wal_sync_method_fsync
+~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_timeout =" "archive_timeout = 10" ~/results/settings/archive_timeout archive_timeout_10
 
-~/scripts/autorun_optimizations.sh "#wal_sync_method =" "wal_sync_method = open_datasync" ~/results/settings/wal_sync_method wal_sync_method_open_datasync
+~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_timeout =" "archive_timeout = 60" ~/results/settings/archive_timeout archive_timeout_60
 
-~/scripts/autorun_optimizations.sh "#wal_sync_method =" "wal_sync_method = open_sync" ~/results/settings/wal_sync_method wal_sync_method_open_sync
+~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_timeout =" "archive_timeout = 100" ~/results/settings/archive_timeout archive_timeout_100
 
-######################################################
-#                    FULL PAGE WRITES                #
-######################################################
-
-~/scripts/autorun_optimizations.sh "#full_page_writes =" "full_page_writes = off" ~/results/settings/full_page_writes full_page_writes_off
-
-######################################################
-#                       WAL BUFFERS                  #
-######################################################
-
-~/scripts/autorun_optimizations.sh "#wal_buffers =" "wal_buffers = 2MB" ~/results/settings/wal_buffers wal_buffers_2MB
-
-~/scripts/autorun_optimizations.sh "#wal_buffers =" "wal_buffers = 4MB" ~/results/settings/wal_buffers wal_buffers_4MB
-
-~/scripts/autorun_optimizations.sh "#wal_buffers =" "wal_buffers = 8MB" ~/results/settings/wal_buffers wal_buffers_8MB
-
-~/scripts/autorun_optimizations.sh "#wal_buffers =" "wal_buffers = 16MB" ~/results/settings/wal_buffers wal_buffers_16MB
-
-~/scripts/autorun_optimizations.sh "#wal_buffers =" "wal_buffers = 32MB" ~/results/settings/wal_buffers wal_buffers_32MB
-
-######################################################
-#                       COMMIT DELAY                 #
-######################################################
-
-~/scripts/autorun_optimizations.sh "#commit_delay =" "commit_delay = 10" ~/results/settings/commit_delay commit_delay_10
-
-~/scripts/autorun_optimizations.sh "#commit_delay =" "commit_delay = 200" ~/results/settings/commit_delay commit_delay_200
-
-~/scripts/autorun_optimizations.sh "#commit_delay =" "commit_delay = 500" ~/results/settings/commit_delay commit_delay_500
-
-~/scripts/autorun_optimizations.sh "#commit_delay =" "commit_delay = 1000" ~/results/settings/commit_delay commit_delay_1000
-
-~/scripts/autorun_optimizations.sh "#commit_delay =" "commit_delay = 1500" ~/results/settings/commit_delay commit_delay_1500
-
-######################################################
-#                       COMMIT SIBLINGS              #
-######################################################
-
-~/scripts/autorun_optimizations.sh "#commit_siblings =" "commit_siblings = 2" ~/results/settings/commit_siblings commit_siblings_2
-
-~/scripts/autorun_optimizations.sh "#commit_siblings =" "commit_siblings = 4" ~/results/settings/commit_siblings commit_siblings_4
-
-~/scripts/autorun_optimizations.sh "#commit_siblings =" "commit_siblings = 8" ~/results/settings/commit_siblings commit_siblings_8
-
-~/scripts/autorun_optimizations.sh "#commit_siblings =" "commit_siblings = 16" ~/results/settings/commit_siblings commit_siblings_16
+~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_timeout =" "archive_timeout = 120" ~/results/settings/archive_timeout archive_timeout_120
 
