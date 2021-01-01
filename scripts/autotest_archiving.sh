b15f343b217ac03eb3209baa7d1cca1e4b10f080
Rui Azevedo <ruiazevedo@MBP-de-Rui.lan>
2021-01-01 19:27:36 +0000

    refactor checkpoint & archiving scripts

--- scripts/autotest_archiving.sh
+++ scripts/autotest_archiving.sh
@@ -4,29 +4,29 @@
   #                   ARCHIVE MODE                   #
 ######################################################
 
-~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_mode =" "archive_mode = off" ~/results/settings/archive_mode archive_mode_off
+~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_mode =" "archive_mode = off" ~/results/archiving/archive_mode archive_mode_off
 
-~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_mode =" "archive_mode = on" ~/results/settings/archive_mode archive_mode_on.dat
+~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_mode =" "archive_mode = on" ~/results/archiving/archive_mode archive_mode_on.dat
 
-~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_mode =" "archive_mode = always" ~/results/settings/archive_mode archive_mode_always
+~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_mode =" "archive_mode = always" ~/results/archiving/archive_mode archive_mode_always
 
 ######################################################
 #                    ARCHIVE COMMAND                 #
 ######################################################
 
-~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_command =" "archive_command = '%p'" ~/results/settings/archive_command archive_command_p
+~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_command =" "archive_command = '%p'" ~/results/archiving/archive_command archive_command_p
 
-~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_command =" "archive_command = '%f'" ~/results/settings/archive_command archive_command_f
+~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_command =" "archive_command = '%f'" ~/results/archiving/archive_command archive_command_f
 
 
 ######################################################
 #                     ARCHIVE TIMEOUT                #
 ######################################################
 
-~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_timeout =" "archive_timeout = 10" ~/results/settings/archive_timeout archive_timeout_10
+~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_timeout =" "archive_timeout = 10" ~/results/archiving/archive_timeout archive_timeout_10
 
-~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_timeout =" "archive_timeout = 60" ~/results/settings/archive_timeout archive_timeout_60
+~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_timeout =" "archive_timeout = 60" ~/results/archiving/archive_timeout archive_timeout_60
 
-~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_timeout =" "archive_timeout = 100" ~/results/settings/archive_timeout archive_timeout_100
+~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_timeout =" "archive_timeout = 100" ~/results/archiving/archive_timeout archive_timeout_100
 
-~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_timeout =" "archive_timeout = 120" ~/results/settings/archive_timeout archive_timeout_120
+~/scripts/auxiliary_scripts/autorun_optimizations.sh server-archiving Joel 80 dump.file 100 "#archive_timeout =" "archive_timeout = 120" ~/results/archiving/archive_timeout archive_timeout_120
