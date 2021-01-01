c1af35d840a156925dbd23541d7500e37f3717ab
Rui Azevedo <ruiazevedo@MBP-de-Rui.lan>
2021-01-01 19:02:44 +0000

    checkpoints & archiving scripts

--- scripts/autotest_archiving.sh
+++ scripts/autotest_archiving.sh
@@ -4,30 +4,30 @@
   #                   ARCHIVE MODE                   #
 ######################################################
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_mode =" "archive_mode = off" ~/results/settings/archive_mode archive_mode_off
+~/scripts/autorun_optimizations.sh server-archiving 80 dump.file 100 "#archive_mode =" "archive_mode = off" ~/results/settings/archive_mode archive_mode_off
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_mode =" "archive_mode = on" ~/results/settings/archive_mode archive_mode_on.dat
+~/scripts/autorun_optimizations.sh server-archiving 80 dump.file 100 "#archive_mode =" "archive_mode = on" ~/results/settings/archive_mode archive_mode_on.dat
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_mode =" "archive_mode = always" ~/results/settings/archive_mode archive_mode_always
+~/scripts/autorun_optimizations.sh server-archiving 80 dump.file 100 "#archive_mode =" "archive_mode = always" ~/results/settings/archive_mode archive_mode_always
 
 ######################################################
 #                    ARCHIVE COMMAND                 #
 ######################################################
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_command =" "archive_command = '%p'" ~/results/settings/archive_command archive_command_p
+~/scripts/autorun_optimizations.sh server-archiving 80 dump.file 100 "#archive_command =" "archive_command = '%p'" ~/results/settings/archive_command archive_command_p
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_command =" "archive_command = '%f'" ~/results/settings/archive_command archive_command_f
+~/scripts/autorun_optimizations.sh server-archiving 80 dump.file 100 "#archive_command =" "archive_command = '%f'" ~/results/settings/archive_command archive_command_f
 
 
 ######################################################
 #                     ARCHIVE TIMEOUT                #
 ######################################################
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_timeout =" "archive_timeout = 10" ~/results/settings/archive_timeout archive_timeout_10
+~/scripts/autorun_optimizations.sh server-archiving 80 dump.file 100 "#archive_timeout =" "archive_timeout = 10" ~/results/settings/archive_timeout archive_timeout_10
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_timeout =" "archive_timeout = 60" ~/results/settings/archive_timeout archive_timeout_60
+~/scripts/autorun_optimizations.sh server-archiving 80 dump.file 100 "#archive_timeout =" "archive_timeout = 60" ~/results/settings/archive_timeout archive_timeout_60
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_timeout =" "archive_timeout = 100" ~/results/settings/archive_timeout archive_timeout_100
+~/scripts/autorun_optimizations.sh server-archiving 80 dump.file 100 "#archive_timeout =" "archive_timeout = 100" ~/results/settings/archive_timeout archive_timeout_100
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#archive_timeout =" "archive_timeout = 120" ~/results/settings/archive_timeout archive_timeout_120
+~/scripts/autorun_optimizations.sh server-archiving 80 dump.file 100 "#archive_timeout =" "archive_timeout = 120" ~/results/settings/archive_timeout archive_timeout_120
 
--- scripts/autotest_checkpoint.sh
+++ scripts/autotest_checkpoint.sh
@@ -6,63 +6,63 @@
 
 ~/scripts/auxiliary_scripts/autorun_optimizations.sh server-checkpoint 80 ~/tpcc80_joel.dump 100 "#fsync =" "fsync = off"  ~/results/clients 80warehouses_10clients
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#checkpoint_timeout =" "checkpoint_timeout = 1min" ~/results/settings/checkpoint_timeout checkpoint_timeout_1min
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "#checkpoint_timeout =" "checkpoint_timeout = 1min" ~/results/settings/checkpoint_timeout checkpoint_timeout_1min
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#checkpoint_timeout =" "checkpoint_timeout = 2min" ~/results/settings/checkpoint_timeout checkpoint_timeout_2min
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "#checkpoint_timeout =" "checkpoint_timeout = 2min" ~/results/settings/checkpoint_timeout checkpoint_timeout_2min
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#checkpoint_timeout =" "checkpoint_timeout = 5min" ~/results/settings/checkpoint_timeout checkpoint_timeout_5min
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "#checkpoint_timeout =" "checkpoint_timeout = 5min" ~/results/settings/checkpoint_timeout checkpoint_timeout_5min
 
 ######################################################
 #                     MAX WAL SIZE                   #
 ######################################################
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "max_wal_size =" "max_wal_size = 2GB" ~/results/settings/max_wal_size max_wal_size_2GB
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "max_wal_size =" "max_wal_size = 2GB" ~/results/settings/max_wal_size max_wal_size_2GB
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "max_wal_size =" "max_wal_size = 3GB" ~/results/settings/max_wal_size max_wal_size_3GB
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "max_wal_size =" "max_wal_size = 3GB" ~/results/settings/max_wal_size max_wal_size_3GB
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "max_wal_size =" "max_wal_size = 4GB" ~/results/settings/max_wal_size max_wal_size_4GB
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "max_wal_size =" "max_wal_size = 4GB" ~/results/settings/max_wal_size max_wal_size_4GB
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "max_wal_size =" "max_wal_size = 5GB" ~/results/settings/max_wal_size max_wal_size_5GB
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "max_wal_size =" "max_wal_size = 5GB" ~/results/settings/max_wal_size max_wal_size_5GB
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "max_wal_size =" "max_wal_size = 6GB" ~/results/settings/max_wal_size max_wal_size_6GB
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "max_wal_size =" "max_wal_size = 6GB" ~/results/settings/max_wal_size max_wal_size_6GB
 
 ######################################################
 #                     MIN WAL SIZE                   #
 ######################################################
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "min_wal_size =" "min_wal_size = 80MB" ~/results/settings/min_wal_size min_wal_size_80MB
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "min_wal_size =" "min_wal_size = 80MB" ~/results/settings/min_wal_size min_wal_size_80MB
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "min_wal_size =" "min_wal_size = 160MB" ~/results/settings/min_wal_size min_wal_size_160MB
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "min_wal_size =" "min_wal_size = 160MB" ~/results/settings/min_wal_size min_wal_size_160MB
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "min_wal_size =" "min_wal_size = 320MB" ~/results/settings/min_wal_size min_wal_size_320MB
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "min_wal_size =" "min_wal_size = 320MB" ~/results/settings/min_wal_size min_wal_size_320MB
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "min_wal_size =" "min_wal_size = 640MB" ~/results/settings/min_wal_size min_wal_size_640MB
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "min_wal_size =" "min_wal_size = 640MB" ~/results/settings/min_wal_size min_wal_size_640MB
 
 ######################################################
 #            CHECKPOINT COMPLETION TARGET            #
 ######################################################
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#checkpoint_completion_target =" "checkpoint_completion_target = 0.0" ~/results/settings/checkpoint_completion_target checpoint_completion_target_00
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "#checkpoint_completion_target =" "checkpoint_completion_target = 0.0" ~/results/settings/checkpoint_completion_target checpoint_completion_target_00
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#checkpoint_completion_target =" "checkpoint_completion_target = 0.2" ~/results/settings/checkpoint_completion_target checpoint_completion_target_02
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "#checkpoint_completion_target =" "checkpoint_completion_target = 0.2" ~/results/settings/checkpoint_completion_target checpoint_completion_target_02
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#checkpoint_completion_target =" "checkpoint_completion_target = 0.5" ~/results/settings/checkpoint_completion_target checpoint_completion_target_05
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "#checkpoint_completion_target =" "checkpoint_completion_target = 0.5" ~/results/settings/checkpoint_completion_target checpoint_completion_target_05
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#checkpoint_completion_target =" "checkpoint_completion_target = 0.8" ~/results/settings/checkpoint_completion_target checpoint_completion_target_08
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "#checkpoint_completion_target =" "checkpoint_completion_target = 0.8" ~/results/settings/checkpoint_completion_target checpoint_completion_target_08
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "#checkpoint_completion_target =" "checkpoint_completion_target = 1.0" ~/results/settings/checkpoint_completion_target checpoint_completion_target_10
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "#checkpoint_completion_target =" "checkpoint_completion_target = 1.0" ~/results/settings/checkpoint_completion_target checpoint_completion_target_10
 
 ######################################################
 #                    CHECKPOINT WARNING              #
 ######################################################
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "checkpoint_warning =" "checkpoint_warning = 0" ~/results/settings/checkpoint_warning checkpoint_warning_0
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "checkpoint_warning =" "checkpoint_warning = 0" ~/results/settings/checkpoint_warning checkpoint_warning_0
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "checkpoint_warning =" "checkpoint_warning = 10s" ~/results/settings/checkpoint_warning checkpoint_warning_10
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "checkpoint_warning =" "checkpoint_warning = 10s" ~/results/settings/checkpoint_warning checkpoint_warning_10
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "checkpoint_warning =" "checkpoint_warning = 30s" ~/results/settings/checkpoint_warning checkpoint_warning_30s
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "checkpoint_warning =" "checkpoint_warning = 30s" ~/results/settings/checkpoint_warning checkpoint_warning_30s
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "checkpoint_warning =" "checkpoint_warning = 60s" ~/results/settings/checkpoint_warning checkpoint_warning_60s
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "checkpoint_warning =" "checkpoint_warning = 60s" ~/results/settings/checkpoint_warning checkpoint_warning_60s
 
-~/scripts/autorun_optimizations.sh server-settings 80 dump.file 100 "checkpoint_warning =" "checkpoint_warning = 120s" ~/results/settings/checkpoint_warning checkpoint_warning_120s
+~/scripts/autorun_optimizations.sh server-checkpoint 80 dump.file 100 "checkpoint_warning =" "checkpoint_warning = 120s" ~/results/settings/checkpoint_warning checkpoint_warning_120s
 
