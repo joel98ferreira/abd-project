06a97adb54e3c4f57274c8556562ce55ab442d89
Joel Ferreira <joel_alexandre98@hotmail.com>
2021-01-03 00:50:09 +0000

    output path fixed

--- scripts/autotest_clients.sh
+++ scripts/autotest_clients.sh
@@ -5,26 +5,26 @@ dbUser=$2
 nrWarehouses=$3
 backupFile=$4
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 10 ~/results/clients "$nrWarehouseswarehouses_10clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 10 ~/results/clients "$nrWarehouses"warehouses_10clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 20 ~/results/clients "$nrWarehouseswarehouses_20clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 20 ~/results/clients "$nrWarehouses"warehouses_20clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 30 ~/results/clients "$nrWarehouseswarehouses_30clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 30 ~/results/clients "$nrWarehouses"warehouses_30clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 40 ~/results/clients "$nrWarehouseswarehouses_40clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 40 ~/results/clients "$nrWarehouses"warehouses_40clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 50 ~/results/clients "$nrWarehouseswarehouses_50clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 50 ~/results/clients "$nrWarehouses"warehouses_50clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 60 ~/results/clients "$nrWarehouseswarehouses_60clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 60 ~/results/clients "$nrWarehouses"warehouses_60clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 70 ~/results/clients "$nrWarehouseswarehouses_70clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 70 ~/results/clients "$nrWarehouses"warehouses_70clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 80 ~/results/clients "$nrWarehouseswarehouses_80clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 80 ~/results/clients "$nrWarehouses"warehouses_80clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 90 ~/results/clients "$nrWarehouseswarehouses_90clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 90 ~/results/clients "$nrWarehouses"warehouses_90clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 100 ~/results/clients "$nrWarehouseswarehouses_100clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 100 ~/results/clients "$nrWarehouses"warehouses_100clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 110 ~/results/clients "$nrWarehouseswarehouses_110clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 110 ~/results/clients "$nrWarehouses"warehouses_110clients
 
-~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 120 ~/results/clients "$nrWarehouseswarehouses_120clients"
+~/scripts/auxiliary_scripts/autorun_clients.sh $dbServerName $dbUser $nrWarehouses $backupFile 120 ~/results/clients "$nrWarehouses"warehouses_120clients
